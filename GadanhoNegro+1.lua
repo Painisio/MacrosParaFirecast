fa8d1913fc2c8a55aeaf5e536f9407f83fd5444b
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-11-20 10:11:44 -0300

    Update GadanhoNegro+1.lua

--- GadanhoNegro+1.lua
+++ GadanhoNegro+1.lua
@@ -8,13 +8,13 @@ Dano_base = rolar("2d4") -- dano da arma puro
 Bba = 0 -- bonus base de ataque
 Mod_hab = 7 -- mod habilidae
 Talento_1 = -2 -- bonus/penalidade de talento
-Crit_marg = 18 -- magem de critico da arma
+Crit_marg = 19 -- magem de critico da arma
 Crit_mult = 4 -- multiplicador de critico da arma
 Bonus_magico = 1 -- bonus magico se houver
 Nome_arma = "Gadanho Negro" -- nome da arma
 
 
-if D20 > Crit_marg then -- checagem se a arma critou...
+if D20 >= Crit_marg then -- checagem se a arma critou...
     Ataque = D20 + Mod_hab + Talento_1 + Bba + Bonus_magico -- calculo da margem de acerto
     Dano_final = ( Dano_base + Bonus_magico + Mod_hab ) * Crit_mult -- calculo do dano da arma
     --prints
